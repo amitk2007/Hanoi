1a1f6020f5232e804f78a2e71dc438aea679db88
amitk2007 <amitk2007@gmail.com>
2016-11-07 17:16:40 +0200

    times 2
    *2

--- Assets/Scripts/GameMangeger.cs
+++ Assets/Scripts/GameMangeger.cs
@@ -19,19 +19,19 @@ public class GameMangeger : MonoBehaviour
 		numberOfRings = numberOfRing;
 		//**********************
 		Hight = TotalRingHight(numberOfRings);
-        RingHight = TotalRingHight(numberOfRings) / (numberOfRings);//to change****
+        RingHight =( TotalRingHight(numberOfRings) / (numberOfRings))*2;//to change****
         //Creating the rings
         
 
 		for (int i = -1; i< 2; i++) 
 		{
-			mot.transform.localScale = new Vector3( 0.2f, TotalRingHight(numberOfRings)+1f	,0.2f);
-            Towers[i+1] = (GameObject)Instantiate(mot, new Vector3(i*((numberOfRings/2.5f)+1f), mot.transform.localScale.y, 0), mot.transform.localRotation);
+			mot.transform.localScale = new Vector3( 0.2f*2, (TotalRingHight(numberOfRings)+1f)*2,0.2f*2);
+            Towers[i+1] = (GameObject)Instantiate(mot, new Vector3((i*((numberOfRings/2.5f)+1f))*2, mot.transform.localScale.y, 0), mot.transform.localRotation);
 		}
 		for (int i = 0; i < numberOfRings; i++)
 		{
 			// Ring scale             = new Vector(        X radios           ,        hight          ,      Y radios             );
-			ring.transform.localScale = new Vector3((numberOfRings - i) / 2.5f, RingHight, (numberOfRings - i) / 2.5f);
+			ring.transform.localScale = new Vector3(((numberOfRings - i) / 2.5f)*2, RingHight, ((numberOfRings - i) / 2.5f)*2);
 			//Create ring (ring properties, hight     ,         Rotation            );
 			//Towers[1].GetComponent<Tower>().Rings.Push((GameObject)Instantiate(ring, new Vector3(0, i * (RingHight+0.2f) + 1f, 0),ring.transform.localRotation));
 			Instantiate(ring, new Vector3(Towers[0].transform.position.x, i * (RingHight+0.2f) + 1f, 0),ring.transform.localRotation);
--- Assets/Scripts/GuiButtonsScript.cs
+++ Assets/Scripts/GuiButtonsScript.cs
@@ -14,9 +14,9 @@ public class GuiButtonsScript : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		if (GUI.Button (new Rect (10, 10, 50, 30), "restart"))
+		if (GUI.Button (new Rect (10, 10, 50*5, 30*5), "restart"))
 			Application.LoadLevel ("game");		
-		if (GUI.Button (new Rect (Screen.width-10-50, 10, 50, 30), "Menu"))
+		if (GUI.Button (new Rect (Screen.width-10-50*5, 10, 50*5, 30*5), "Menu"))
 			Application.LoadLevel ("menu");		
 	}
 }
